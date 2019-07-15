73d6d45f4fa6834a8ff95c3e9350f2a182f8ffdf
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-07-15 14:08:20 +0200

    Updates example of working with pimsclient TRAC#8671

--- examples/pseudonymize_reidentify_with_project.py
+++ examples/pseudonymize_reidentify_with_project.py
@@ -5,11 +5,22 @@ from pimsclient.client import connect, PatientID, PseudoPatientID
 
 # Create a project connected to a certain PIMS key file
 project = connect('https://pims.radboudumc.nl/api', pims_key_file_id=26)
+print(project.get_pims_pseudonym_template())
 
 # I have some patientID's I want to pseudonymize with PIMS
-keys = project.pseudonymize([PatientID('1234'), PatientID('5678'), PatientID('9012')])
+print("Pseudonymizing some patientIDs: ")
+keys1 = project.pseudonymize([PatientID('5123'), PatientID('5124'), PatientID('5125')])
+print([str(x.identifier) + " - " + str(x) for x in keys1])
 
-# Other way around: I found some pseudonymized patientID's. What was the original ID?
-keys = project.reidentify([PseudoPatientID('Patient1'),
-                           PseudoPatientID('Patient2'),
-                           PseudoPatientID('Patient3')])
+# extract the pseudonyms to use as example in next step
+pseudo_patient_name1 = keys1[0].pseudonym.value
+pseudo_patient_name2 = keys1[1].pseudonym.value
+pseudo_patient_name3 = keys1[2].pseudonym.value
+
+# Next step, Other way around: I found some pseudonymized patientID's. What was the original ID?
+print("Reidentifying the pseudonyms:")
+keys2 = project.reidentify([PseudoPatientID(pseudo_patient_name1),
+                            PseudoPatientID(pseudo_patient_name2),
+                            PseudoPatientID(pseudo_patient_name3)])
+print([str(x) + " - " + str(x.identifier) for x in keys2])
+
