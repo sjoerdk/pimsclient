3026ad3e06832b5a8114362871b05a09b64d6373
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-09-10 14:11:58 +0200

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sjoerdk/pimsclient',
-    version='0.3.0',
+    version='version="0.3.1"',
     zip_safe=False,
 )
